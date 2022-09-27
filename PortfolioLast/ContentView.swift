eb38797598dc99149b90e3a7d2a4ea0ac7132459
Владислав <92315082+Vladislavrudnev@users.noreply.github.com>
2022-09-27 09:24:53 +0600

    Initial Commit

---
+++ PortfolioLast/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PortfolioLast
+//
+//  Created by владислав Руднев on 27.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PortfolioLast/PortfolioLastApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioLastApp.swift
+//  PortfolioLast
+//
+//  Created by владислав Руднев on 27.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioLastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
